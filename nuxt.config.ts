57293bb12b46a1eea42f41d676abac4b86d78671
Aaron Kow <AaronKow@users.noreply.github.com>
2025-01-01 14:16:31 +0800

    updated meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,48 @@
+const title = 'PandaのStory';
+const desc = 'Pixel World of Pandas, Stories, and Connections.';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   app: {
     head: {
+      title,
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
+      meta: [
+        {
+          name: 'description',
+          content: desc,
+        },
+      ],
       link: [
+        // Favicons
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon.ico' },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon/favicon-16x16.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon/favicon-32x32.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/favicon/apple-touch-icon.png',
+        },
+        {
+          rel: 'manifest',
+          href: '/favicon/site.webmanifest',
+        },
+
+        // Fonts
         {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com',
@@ -24,6 +61,5 @@ export default defineNuxtConfig({
   },
 
   modules: ['@nuxtjs/tailwindcss'],
-
   css: ['~/assets/styles/global.css'],
 });
